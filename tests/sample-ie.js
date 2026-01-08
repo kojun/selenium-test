f240a517b4954e0ff04d9548eae9867c6011aa30
三井不動産リアルティ 寺井 恒順 <X54843@internal.mf-realty.jp>
2026-01-08 16:49:30 +0900

    sample-ie.jsをちゃんと動くようにした

--- tests/sample-ie.js
+++ tests/sample-ie.js
@@ -6,13 +6,20 @@ const {Options} = require('selenium-webdriver/ie');
   ieOptions.setEdgeChromium(true);
   ieOptions.setEdgePath('C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe');
 
+  ieOptions.ignoreZoomSetting(true);  //ズーム100%問題を回避
+  ieOptions.initialBrowserUrl("https://nikkei.com");  // 遷移ハング回避の「効くことがある」手
+
   let driver = await new Builder().
                          forBrowser('ie').
                          setIeOptions(ieOptions).
                          build();
   try {
+    console.log("BEFORE get");
     await driver.get('http://nikkei.com');
-    await driver.wait(until.titleContains("日本経済新聞"), 5000);
+    console.log("AFTER get");
+
+    // titleはIEモードだと変わる or 遅いことがあるので少し長めに
+    await driver.wait(until.titleContains("日本経済新聞"), 20000);
     console.log("TITLE: " + await driver.getTitle());
   } finally {
     await driver.quit();
